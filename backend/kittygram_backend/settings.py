7dd12ef675ef97902a404deac99d678f8a355117
Сафонов Сергей <sergey_safonov86@inbox.ru>
2023-12-08 19:01:09 +0300

    Fix to first review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,8 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv(
-    'SECRET_KEY', '-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4')
+SECRET_KEY = os.getenv('SECRET_KEY', '-cg6*%6d51ef8f#4!r3*$vmxm4)abgj!4')
 
 DEBUG = os.getenv('DEBUG', '').lower() == 'true'
 
